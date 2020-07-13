624ae95c2eef08de64571c1ee3b56a598e652568
PaulBigMouth <strashnipal@mail.ru>
2020-07-13 15:07:40 +0300

    1

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">Семен</h1>
+            <p className="lead">Семен лучший повар на планете</p>
+        </div>
+    </div>
+)
